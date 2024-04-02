972f5710de858f0905a4ab63f6739b3460e127d5
DavideTarantino <davidetarantino2005@gmail.com>
2024-04-02 14:06:34 +0200

    correzione migration

--- database/migrations/2024_04_02_100237_add_type_id_to_projects_table.php
+++ database/migrations/2024_04_02_100237_add_type_id_to_projects_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->string('type_id')->after('id');
+            $table->unsignedBigInteger('type_id')->after('id');
+
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
         });
     }
 
@@ -22,7 +24,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
         });
     }
 };
